2381b34584c0db39f7ddb79b30f0b1f308384bed
ANDRIANANTENAINA <herynambininaa@gmail.com>
2022-03-23 13:58:18 +0100

    create different files and struct brands

---
+++ codingCplus/main.cpp
@@ -0,0 +1,17 @@
+/*Author Hery ANDRIANANTENAINA
+*this program allows to read the number of items stored in a binary file and to sum the same data
+*
+*Execution:
+*./bin [file]
+*file  : binary file
+*
+*/
+#include<iostream>
+#include"utils.h"
+
+
+int main()
+{
+
+    return 0;
+}
---
+++ codingCplus/utils.hpp
@@ -0,0 +1,17 @@
+/**hedear file*/
+
+#ifndef UTILS_H
+#define UTILS_H
+
+using namespace std;
+
+const int MAXBUFFEUR = 50;
+
+struct
+{
+    int number;
+    int value;
+    char nom[MAXBUFFEUR];
+}brands;
+
+#endif
